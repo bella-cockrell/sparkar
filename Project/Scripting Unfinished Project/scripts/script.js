bb5a81cca3c1f59c78339adf629255fa34025fd4
Bella <arabella.cockrell@gmail.com>
2021-12-03 16:14:31 +0000

    Adding animation for base of boombox - it now jiggles

--- Project/Scripting Unfinished Project/scripts/script.js
+++ Project/Scripting Unfinished Project/scripts/script.js
@@ -1,2 +1,22 @@
+const Animation = require('Animation');
 const Scene = require('Scene');
 
+Scene.root.findFirst('base_jnt')
+.then(function(base) {
+  const baseDriverParameters = {
+  durationMilliseconds: 400,
+    loopCount: Infinity,
+    mirror: true
+  };
+
+  const baseDriver = Animation.timeDriver(baseDriverParameters);
+  baseDriver.start();
+  const baseSampler = Animation.samplers.easeInQuint(0.9, 1);
+  const baseAnimation = Animation.animate(baseDriver, baseSampler);
+
+  const baseTransform = base.transform;
+
+  baseTransform.scaleX = baseAnimation;
+  baseTransform.scaleY = baseAnimation;
+  baseTransform.scaleZ = baseAnimation;
+});
